13b0da2f3949cc94d7ce7d61f651cd3d26ee69f3
Jacob Chvatal <jake@isnt.online>
2021-03-23 11:43:47 -0700

    add incomplete nix build

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "framework for making board games";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        inherit (lib) attrValues;
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = nixpkgs.lib;
+        package = with pkgs; callPackage ./. { inherit pkgs; };
+      in { defaultPackage = package; });
+}
